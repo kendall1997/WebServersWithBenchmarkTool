853f6d16401a2cf5829ea70cf3ca33b705888995
Kendall Gonzalez Leon <kendall.gonzalez@driverkey.net>
2019-04-05 19:26:54 -0600

    Last changes made

--- WebServerFIFO/DockerScripts/THREADED/start.sh
+++ WebServerFIFO/DockerScripts/THREADED/start.sh
@@ -3,15 +3,6 @@
 # Load set up
 . ./environment.sh
 
-# Lets compile the software in the host machine using our recipe in the Makefile
-echo "--------------------------------------------"
-echo "Starting WebServer"
-echo "--------------------------------------------"
-#/bin/sh
-
-# Load set up
-. ./environment.sh
-
 echo "--------------------------------------------"
 echo "Starting Container $BIN_NAME"
 echo "--------------------------------------------"
