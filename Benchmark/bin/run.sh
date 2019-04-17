77616acecbf7ef5b6452742f53254c72271e121b
Kendall Gonzalez Leon <kendall.gonzalez@driverkey.net>
2019-04-17 12:24:13 -0600

    Last version of benchmark

--- Benchmark/bin/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-touch results2.csv
-
-for threads in {2..5}
-do
-   echo -e "Downloading file with $threads threads\n" >> results_victor_2.csv
-   ./bclient autogestion.metrotel.com.ar 80 speedtest/archivo10MB.zip $threads 1 >> results_victor_2.csv 
-done
