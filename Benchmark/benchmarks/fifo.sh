a29a0b069edb3faa2dff7302c4a4015541120f06
Victor Chavarria Fernandez <victor2044@hotmail.es>
2019-04-18 04:45:15 -0600

    update of bash

--- Benchmark/benchmarks/fifo.sh
+++ Benchmark/benchmarks/fifo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+. general.sh
 
-FILE=fifo_test.csv
+FILE=Benchmark_1_FIFO.csv
 
 touch $FILE
 
 for threads in {2..2}
 do
    echo -e "Downloading file with $threads threads\n" >> $FILE
-   ./bclient 192.168.1.111 8001 rocket.webm $threads 1 >> $FILE 
+   ./bclient $IP $Port $N_File $threads 1 >> $FILE 
 done
