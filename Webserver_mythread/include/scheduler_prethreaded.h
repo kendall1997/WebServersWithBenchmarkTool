0bed024402dc65135bf4bf1c29062cefadfb4a8b
nabetse28 <nabetse.hv.09@gmail.com>
2019-04-23 02:24:20 -0600

    Webserver thread

---
+++ Webserver_mythread/include/scheduler_prethreaded.h
@@ -0,0 +1,32 @@
+// Costa Rica Institute of Technology
+// Computer Engineering
+// Operating Systems
+// Kendall González León
+// 2015087861
+
+#ifndef SCHEDULER_PRETHREADED
+#define SCHEDULER_PRETHREADED
+
+#define FALSE 0
+#define TRUE 1
+
+#include <queue.h>
+#include <pthread.h>
+#include <../../mythreads/mythread.h>
+#include <../../mythreads/mymutex.h>
+
+static struct Queue *prethreaded_request;
+static mythread_t *threadPool;
+static int prethreaded_started = FALSE;
+static int prethreaded_working_count;
+static int *prethreaded_slots;
+static int prethreaded_poolSize;
+static mythread_mymutex_t lockWrite;
+static mythread_mymutex_t lockRead;
+
+void scheduler_prethreaded(int slot, struct Queue *requests, int poolSize);
+void *prethreaded_task(void *package);
+void prethreaded_work(int slot);
+void prethreaded_after_work(int threadId);
+
+#endif
---
+++ Webserver_mythread/src/config.c
@@ -0,0 +1,74 @@
+// Costa Rica Institute of Technology
+// Computer Engineering
+// Operating Systems
+// Kendall González León
+// 2015087861
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <config.h>
+
+/**
+ * @brief      Gets the configuration from file.
+ *
+ * @param      filename  The filename to look into
+ *
+ * @return     The configuration struct.
+ */
+struct config get_config(char const *filename){
+  struct config configstruct;
+  FILE *file = fopen (filename, "r");
+
+  char pool_buffer[MAXBUF];
+
+  if (file != NULL){ 
+    char line[MAXBUF];
+    int i = 0;
+
+    while(fgets(line, sizeof(line), file) != NULL){
+      char *cfline;
+      cfline = strstr((char *)line,DELIM);
+      cfline = cfline + strlen(DELIM);
+
+      if (i == 0){
+        memcpy(configstruct.port,cfline,strlen(cfline));
+        //configstruct.port[strlen(cfline)-1] = NULL;
+        configstruct.port[strcspn(configstruct.port, "\n")] = '\0';
+        //printf("%s\n",configstruct.port);
+      } else if (i == 1){
+        memcpy(configstruct.folder,cfline,strlen(cfline));
+        //configstruct.folder[strlen(cfline)-1] = NULL;
+        configstruct.folder[strcspn(configstruct.folder, "\n")] = '\0';
+        //printf("%s\n",configstruct.folder);
+      } else if (i == 2){
+        if(strcmp(line, "\n") == 0){
+          char* syspath = "/var/log/syslog";
+          memcpy(configstruct.log,syspath,strlen(syspath));
+        }else{
+          memcpy(configstruct.log,cfline,strlen(cfline));
+          //configstruct.log[strlen(cfline)-1] = NULL;
+          configstruct.log[strcspn(configstruct.log, "\n")] = '\0';
+          //printf("%s\n",configstruct.log);          
+        }
+      } else if (i == 3){
+        memcpy(configstruct.scheduler,cfline,strlen(cfline));
+        //configstruct.folder[strlen(cfline)-1] = NULL;
+        configstruct.scheduler[strcspn(configstruct.scheduler, "\n")] = '\0';
+        //printf("%s\n",configstruct.folder);
+      } else if (i == 4){
+        memcpy(pool_buffer,cfline,strlen(cfline));
+        //configstruct.folder[strlen(cfline)-1] = NULL;
+        pool_buffer[strcspn(pool_buffer, "\n")] = '\0';
+        //printf("%s\n",configstruct.folder);
+      }
+      i++;
+    } // End while
+    fclose(file);
+  } // End if file
+
+  configstruct.pool = atoi(pool_buffer);
+  
+  return configstruct;
+}
