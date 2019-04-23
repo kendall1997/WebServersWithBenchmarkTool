6a599cea99fee5603710068f086e0913f53107d5
Victor Chavarria Fernandez <victor2044@hotmail.es>
2019-04-22 21:41:35 -0600

    Comment update

--- Benchmark/include/agent.h
+++ Benchmark/include/agent.h
@@ -4,6 +4,8 @@
 
 #include <stdio.h> 
 
+//Struct that stores the information of the requests of server
+
 struct summary {
   char* TypeOfFile;
   char* name;
@@ -15,12 +17,16 @@ struct summary {
   int ResponseTime;
 };
 
+//Struct that stores the data received
 struct FtpFile {
   const char *filename;
   FILE *stream;
 };
 
+//Function that stores the data received
 static size_t my_fwrite(void *buffer, size_t size, size_t nmemb, void *stream);
+
+//Struct that is responsible for making requests to the server and return the requested data
 struct summary* pull(char* url);
 
 #endif
